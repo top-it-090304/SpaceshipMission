83adf13acb0c60612e9ea8e6a1bc13ce52946ffa
Margarita Bokach <rita.bokach@gmail.com>
2026-02-13 11:49:42 +0600

    Godot files added

---
+++ spaceshipmissiontest/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0  
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * SPEED
+
+	move_and_slide()
