c6a0ecdfe8b06c0a2d5e44d814410fd1b777450c
Sofia Babikova <babikovasofia51@gmail.com>
2026-02-13 21:15:26 +0600

    images folder added

--- spaceshipmissiontest/player.gd
+++ spaceshipmissiontest/player.gd
@@ -1,21 +1,26 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0  
+@export var speed: float = 300.0
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var target: Vector2
 
-func _physics_process(delta):
+func _ready() -> void:
+	target = global_position
 
-	if not is_on_floor():
-		velocity.y += gravity * delta
+func _input(event: InputEvent) -> void:
+	# одно нажатие мыши
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()  # мировая позиция клика [web:36]
 
-	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _physics_process(delta: float) -> void:
+	# двигаемся к цели только по X
+	var desired := Vector2(target.x, global_position.y)
 
-	
-	var direction = Input.get_axis("ui_left", "ui_right")
-	velocity.x = direction * SPEED
+	# если ещё далеко – идём
+	if global_position.distance_to(desired) > 2.0:
+		var dir: Vector2 = global_position.direction_to(desired) # нормализованное направление [web:30]
+		velocity = dir * speed
+	else:
+		velocity = Vector2.ZERO  # остановка ровно в точке
 
 	move_and_slide()
