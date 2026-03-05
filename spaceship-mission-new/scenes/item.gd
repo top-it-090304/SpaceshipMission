dd51cbb17c9cf3d52c70b609309a716e54da3e8d
Sofia Babikova <babikovasofia51@gmail.com>
2026-03-05 22:39:58 +0600

    new files added

--- spaceship-mission-new/scenes/item.gd
+++ spaceship-mission-new/scenes/item.gd
@@ -9,8 +9,8 @@ func _ready() -> void:
 	input_event.connect(_on_input_event)
 
 func _on_input_event(viewport, event: InputEvent, shape_idx: int) -> void:
-	if event.is_action_pressed("click"):   # действие "click" = ЛКМ
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		var main_game := get_tree().get_first_node_in_group("MainGame")
 		if main_game:
 			main_game.inventory.add_item(item_id)
-		queue_free()   # убрать предмет из комнаты
+		queue_free()
